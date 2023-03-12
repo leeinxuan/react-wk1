1340b55337881dbc3b6b0df976d60144064015dc
leeinxuan <x14725836961@gmail.com>
2023-03-12 12:47:18 +0800

    增加antd+CSS Grid

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import { Row, Col } from "antd"
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__above">
+        
+          <Row  type="flex" justify="center" align="middle">
+            <Col
+              sm={{ span: 24 }}
+              lg={{ span: 12 }}
+              xl={{ span: 12 }}
+              xxl={{ span: 12 }}
+            >
+              <h3 className="footer__description">
+                臺北市和平東路二段134號，國立臺北教育大學 數位科技設計學系
+              </h3>
+            </Col>
+            <Col
+              sm={{ span: 24 }}
+              lg={{ span: 12 }}
+              xl={{ span: 12 }}
+              xxl={{ span: 12 }}
+            >
+              <ul className="footer__list">
+                <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-facebook-f"></i></a></li>
+                <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-google-plus-g"></i></a></li>
+                <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-linkedin-in"></i></a></li>
+              </ul>
+            </Col>
+          </Row>
+        
+      </div>
+
+      <div className="footer__below">
+        <div className="container">
+          <h4>
+            &copy; Copyright All rights reserved.
+          </h4>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from"../components/Header"
+import ProductList from"../components/ProducList"
+import Footer from"../components/Footer"
+function Home(){
+    return(
+        <div className="main-layout">
+            <Header className="layout-header"/>
+            <ProductList className="layout-content"/>
+            <Footer className="layout-footer"/>
+        </div>
+    );
+}
+
+export default Home;
